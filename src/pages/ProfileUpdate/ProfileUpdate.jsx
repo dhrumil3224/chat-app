65c6d5dc1074fbe19157fda320ab3fd642d2e430
dhrumil sakariya <dhrumilsakariya34gmail.com>
2025-04-25 10:11:36 -0400

    setup the project

---
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProfileUpdate.css'
+
+const ProfileUpdate = () => {
+  return (
+    <div>
+      ProFileUpdate
+    </div>
+  )
+}
+
+export default ProfileUpdate
